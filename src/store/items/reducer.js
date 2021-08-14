2a5955fd1fd12b07a552a8537d1f5c854ad1a90a
Rene Corrales <ruckonic@icloud.com>
2021-08-13 20:45:13 -0600

    refactor produce

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -7,40 +7,27 @@ import {
 } from './actions';
 let id = 1;
 
-export const initialItems = [
-  { uuid: id++, name: 'Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan Ham', price: 12, quantity: 1 }
-];
+export const initialItems = [];
 
-export const reducer = (state = initialItems, action) => {
+export const reducer = produce((state = initialItems, action) => {
   if (action.type === ADD_ITEM) {
-    return produce(state, (draft) => {
-      draft.push({ uuid: id++, quantity: 1, ...action.payload });
-    });
+    state.push({ uuid: id++, quantity: 1, ...action.payload });
   }
 
   if (action.type === REMOVE_ITEM) {
-    return produce(state, (draft) => {
-      const idx = draft.findIndex((item) => item.uuid === action.payload.uuid);
-      if (idx !== -1) draft.splice(idx, 1);
-    });
+    const idx = state.findIndex((item) => item.uuid === action.payload.uuid);
+    if (idx !== -1) state.splice(idx, 1);
   }
 
   if (action.type === UPDATE_ITEM_PRICE) {
-    return produce(state, (draft) => {
-      const idx = draft.findIndex((item) => item.uuid === action.payload.uuid);
-      if (idx !== -1) draft[idx].price = action.payload.price;
-    });
+    const idx = state.findIndex((item) => item.uuid === action.payload.uuid);
+    if (idx !== -1) state[idx].price = action.payload.price;
   }
 
   if (action.type === UPDATE_ITEM_QUANTITY) {
-    return produce(state, (draft) => {
-      const idx = draft.findIndex((item) => item.uuid === action.payload.uuid);
-      if (idx !== -1) draft[idx].quantity = action.payload.quantity;
-    });
+    const idx = state.findIndex((item) => item.uuid === action.payload.uuid);
+    if (idx !== -1) state[idx].quantity = action.payload.quantity;
   }
-
-  return state;
-};
+}, initialItems);
 
 export default reducer;
